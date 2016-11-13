b63bc6e0623263dd6c44c8708aea9fd56b050682
Esa Firman <esafirm21@gmail.com>
2016-11-13 22:40:25 +0700

    Add sample module

--- kommonsample/src/main/java/com/esafirm/kommonsample/MainActivity.kt
+++ kommonsample/src/main/java/com/esafirm/kommonsample/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.esafirm.kommonsample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.ViewGroup
+import android.widget.Button
+import com.esafirm.kommon.start
+import com.esafirm.kommon.toast
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        intent.extras?.getString("test")?.let {
+            toast("Extra Value $it")
+        }
+
+        setContentView(Button(this).apply {
+            text = "Press Me"
+            layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+            setOnClickListener {
+                start<MainActivity> {
+                    putExtra("test", "This is a value")
+                }
+            }
+        })
     }
 }
