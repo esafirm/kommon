06b0271a6d626bcb56f465357ee50f50b2f88b49
Esa Firman <esafirm21@gmail.com>
2017-01-16 09:46:10 +0700

    Bump Gradle plugin version
    afterMeasure() on Views

--- kommon/src/main/kotlin/com/esafirm/kommon/Views.kt
+++ kommon/src/main/kotlin/com/esafirm/kommon/Views.kt
@@ -1,5 +1,23 @@
 package com.esafirm.kommon
 
 import android.view.View
+import android.view.ViewGroup
+import android.view.ViewTreeObserver
 
 inline fun <reified T : View> View.find(id: Int): T = findViewById(id) as T
+
+fun <T : View> T.afterMeasured(f: T.() -> Unit) {
+    viewTreeObserver.addOnPreDrawListener(object : ViewTreeObserver.OnPreDrawListener {
+        override fun onPreDraw(): Boolean {
+            viewTreeObserver.removeOnPreDrawListener(this)
+            f()
+            return false
+        }
+    })
+}
+
+fun View.removeFromParent() {
+    if (parent is ViewGroup) {
+        (parent as ViewGroup).removeView(this)
+    }
+}
