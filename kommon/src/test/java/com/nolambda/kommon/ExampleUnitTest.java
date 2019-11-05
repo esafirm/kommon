8418f428468b74937f8c85f7361c32e04fabffe4
Esa Firman <esafirm21@gmail.com>
2019-11-06 01:25:08 +0700

    Fix build error

--- kommon/src/test/java/com/nolambda/kommon/ExampleUnitTest.java
+++
@@ -1,9 +0,0 @@
-package com.nolambda.kommon;
-
-import static junit.framework.Assert.assertEquals;
-
-public class ExampleUnitTest {
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
-}
