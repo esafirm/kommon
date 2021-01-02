74ab79c8d3d2ce624d6c667467db3c5396b2b4f5
Esa Firman <esafirm21@gmail.com>
2021-01-02 17:38:48 +0700

    add viewbinding module

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 rm settings.gradle
 echo "include ':kommon'" >> settings.gradle
 echo "include ':kommonviews'" >> settings.gradle
-echo "include ':kommoncompoundview'" >> settings.gradle
 echo "include ':kommonadapter'" >> settings.gradle
+echo "include ':kommonadapter-viewbinding'" >> settings.gradle
